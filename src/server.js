c9cea5264e44e5bea59944d30d63da0a0737fa6b
@anhnv99 <vietanhhappy99@gmail.com>
2023-02-26 15:03:04 +0700

    create the connection to database and simple query

--- src/server.js
+++ src/server.js
@@ -5,6 +5,9 @@ const path = require("path"); // commonjs
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
 
+// get the client
+const mysql = require("mysql2");
+
 const app = express(); // app express
 const port = process.env.PORT || 8888; // port
 const hostname = process.env.HOST_NAME;
@@ -14,6 +17,22 @@ configViewEngine(app);
 
 // khai báo route
 app.use("/", webRoutes); // Tham số đầu tiên là tiền tố định nghĩa route
+
+// test connection
+// create the connection to database
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3307, //default: 3306
+  user: "root", //default: empty
+  password: "123456",
+  database: "hoidanit",
+});
+
+// simple query
+connection.query("select * from Users u", function (err, results, fields) {
+  console.log("check results >>>", results); // results contains rows returned by server
+  console.log("check fields >>>", fields); // fields contains extra meta data about results, if available
+});
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
