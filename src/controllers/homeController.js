18946f36522da8cf2189be0b36d2f77a8e0649ae
@anhnv99 <vietanhhappy99@gmail.com>
2023-03-03 14:36:33 +0700

    query list users to the interface

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,8 @@
 const connection = require("../config/database");
-
-const getHomePage = (req, res) => {
-  return res.render("home.ejs");
+const { getAllUsers } = require("../services/CRUDService");
+const getHomePage = async (req, res) => {
+  let results = await getAllUsers();
+  return res.render("home.ejs", { listUsers: results });
 };
 
 const getCreatePage = (req, res) => {
