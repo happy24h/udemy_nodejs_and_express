64549fa60408185d4a5bccad33d1364aaba976ce
unknown <anhnv@rubicontechno.com>
2023-03-04 14:08:36 +0700

    note sql

--- src/services/CRUDService.js
+++ src/services/CRUDService.js
@@ -1,7 +1,6 @@
 const connection = require("../config/database");
 const getAllUsers = async () => {
   let [results, fields] = await connection.query("select * from Users");
-  //   console.log(">>> check rows: ", results);
   return results;
 };
 
