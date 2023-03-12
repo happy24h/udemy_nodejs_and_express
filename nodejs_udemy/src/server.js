3ade18730091c09879b1d24d6429916e76048efb
@anhnv99 <vietanhhappy99@gmail.com>
2023-03-12 22:16:08 +0700

    note create database

--- nodejs_udemy/src/server.js
+++ nodejs_udemy/src/server.js
@@ -21,12 +21,15 @@ configViewEngine(app);
 // khai báo route
 app.use("/", webRoutes); // Tham số đầu tiên là tiền tố định nghĩa route
 
+// shape data - xác định hình dạng của các tài liệu trong bộ sưu tập đó.
 const kittySchema = new mongoose.Schema({
   name: String,
 });
 
-const Kitten = mongoose.model("Kitten", kittySchema);
-const cat = new Kitten({ name: "Viet anh hoc backend" });
+// create modal - Tạo ra bảng
+const createModal = mongoose.model("Kitten", kittySchema); // tạo bảng có tên là Kitten và trong mongoose sẽ chuyển thành kittens
+// tạo mới đối tượng kế thừa lại thuộc tính createModal
+const cat = new createModal({ name: "Viet anh hoc backend" });
 cat.save();
 
 (async () => {
