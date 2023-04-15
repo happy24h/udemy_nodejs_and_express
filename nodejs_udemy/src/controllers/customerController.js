2ef812d99ceac8193b7bf7e25cb864c0bbaa3e9c
unknown <anhnv@rubicontechno.com>
2023-04-15 16:29:57 +0700

    validate data

--- nodejs_udemy/src/controllers/customerController.js
+++ nodejs_udemy/src/controllers/customerController.js
@@ -8,34 +8,48 @@ const {
   deleteACustomerService,
   deleteArrayCustomerService,
 } = require("../services/customerService");
+const Joi = require("joi");
 
 // {key: value, key1: value1}
 module.exports = {
   postCreateCustomer: async (req, res) => {
     let { name, address, phone, email, description } = req.body;
-    let imageUrl = "";
 
-    console.log(">>> name:", name);
-    if (!req.files || Object.keys(req.files).length === 0) {
-    } else {
-      let result = await uploadSingleFile(req.files.image);
-      imageUrl = result.path;
-    }
-
-    let customerData = {
-      name,
-      address,
-      phone,
-      email,
-      description,
-      image: imageUrl,
-    };
-
-    let customer = await createCustomerService(customerData);
-    return res.status(200).json({
-      EC: 0,
-      data: customer,
+    const schema = Joi.object({
+      name: Joi.string().alphanum().min(3).max(30).required(),
+      address: Joi.string(),
+      phone: Joi.string().pattern(new RegExp("^[0-9]{8, 11}$")),
+      email: Joi.string().email(),
+      description: Joi.string(),
     });
+    const { error } = schema.validate(req.body);
+    // console.log(">>> check result: ", error);
+    if (error) {
+      // return error
+    } else {
+      let imageUrl = "";
+      console.log(">>> name:", name);
+      if (!req.files || Object.keys(req.files).length === 0) {
+      } else {
+        let result = await uploadSingleFile(req.files.image);
+        imageUrl = result.path;
+      }
+
+      let customerData = {
+        name,
+        address,
+        phone,
+        email,
+        description,
+        image: imageUrl,
+      };
+
+      let customer = await createCustomerService(customerData);
+      return res.status(200).json({
+        EC: 0,
+        data: customer,
+      });
+    }
   },
 
   postCreateArrayCustomerService: async (req, res) => {
